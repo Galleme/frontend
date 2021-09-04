48023fba7c8fff5278a519d736eeaa4f207f173d
Benedikt Kaiser <benediktkaiser8@gmail.com>
2021-09-04 02:14:15 +0200

    (setup) fixed babel error

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,8 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   "plugins": [
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }],
   ]
 }
