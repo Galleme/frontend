f1cb83631aa875bacd12cf8de04d1ea98f41163a
DaanSchar <Daan.Schaer@Gmail.com>
2021-09-05 19:58:13 +0200

    removed other template versions from stories

--- src/stories/common/ButtonWithIcon.stories.js
+++ src/stories/common/ButtonWithIcon.stories.js
@@ -27,31 +27,3 @@ Primary.args = {
     size: 'text-md',
     icon: faStar,
 };
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-    type: 'secondary',
-    size: 'text-md',
-    icon: faStar,
-};
-
-export const Success= Template.bind({});
-Success.args = {
-    type: 'success',
-    size: 'text-md',
-    icon: faStar,
-};
-
-export const Danger = Template.bind({});
-Danger.args = {
-    type: 'danger',
-    size: 'text-md',
-    icon: faStar,
-};
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-    disabled: true,
-    size: 'text-md',
-    icon: faStar,
-};
