50abb8cb7e5a06129bc72824ee44442169433987
DaanSchar <72268586+DaanSchar@users.noreply.github.com>
2021-09-05 23:04:10 +0200

    (feat) create ButtonWithIcon Component (#21)
    * created vue component file
    
    * added to storybooks
    
    * passing props to basebutton
    
    * passing props to basebutton
    
    * not finished thing
    
    * added seperate slots
    
    * removed other template versions from stories
    
    * removed empty style component thingy
    
    * added icon reference to setup
    
    * added templates to story
    
    * added imports/ other suggestions
    
    * Fixed the Icon display issue in ButtonWithIcon.stories.js
    
    * fixed typo
    
    * Fixed Button display
    
    * centered items
    
    * removed brake-line
    
    * removed computed block
    
    * changed stories to template block
    
    Co-authored-by: Benedikt Kaiser <benediktkaiser8@gmail.com>

---
+++ src/components/common/ButtonWithIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <BaseButton :type="type" :disabled="disabled" :size="size">
+    <div class="flex items-center">
+      <slot name="icon"/>
+      <slot name="text"/>
+    </div>
+  </BaseButton>
+</template>
+
+<script>
+import BaseButton from "../../components/common/BaseButton";
+
+export default {
+  name: "ButtonWithIcon",
+  components: {BaseButton},
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'primary',
+      validator: function (value) {
+        return ['primary', 'secondary', 'success', 'danger'].indexOf(value) !== -1;
+      },
+    },
+    size: {
+      type: String,
+      default: '',
+    },
+  }
+}
+</script>
---
+++ src/stories/common/ButtonWithIcon.stories.js
@@ -0,0 +1,38 @@
+import ButtonWithIcon from "../../components/common/ButtonWithIcon";
+import {faFlag, faPlus, faStar} from "@fortawesome/free-solid-svg-icons";
+import Fa from 'vue-fa'
+
+export default {
+    title: 'Common/ButtonWithIcon',
+    component: ButtonWithIcon, Fa,
+    argTypes: {
+        type: {
+            control: { type: 'select' },
+            options: ['primary', 'secondary', 'success', 'danger']
+        },
+        onClick: { action: 'clicked' },
+    },
+};
+
+const Template = (args) => ({
+    components: {
+        ButtonWithIcon,
+        Fa,
+    },
+    setup() {
+        return { faStar, faFlag, faPlus, args };
+    },
+    template:
+        `<button-with-icon v-bind="args">
+            <template #text>Test Button</template>
+            <template #icon>
+                <fa class="text-blue-400 mr-3" size="2x" :icon="faFlag"/>
+            </template>
+        </button-with-icon>`,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+    type: 'primary',
+    size: 'text-md',
+};
