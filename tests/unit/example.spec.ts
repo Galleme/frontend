f19a3bbc65d631f786e0cd7da81eae1748e80d67
Benedikt Kaiser <benediktkaiser8@gmail.com>
2021-09-12 00:22:02 +0200

    Updated Unit Tests

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -4,6 +4,6 @@ import Index from '../../src/views/index.vue'
 describe('Render Homepage', () => {
   it('renders home page', () => {
     const wrapper = mount(Index)
-    expect(wrapper.text()).toMatch('Index Page')
+    expect(wrapper.text()).toMatch('Using Icons:  Test Text (for Faker): Welcome to the index page')
   })
 })
