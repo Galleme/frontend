fe7d977af7113265c24edd0352788da99dac326b
DaanSchar <Daan.Schaer@Gmail.com>
2021-09-12 01:33:23 +0200

    made icon be declared from within button

--- src/components/common/ButtonWithIcon.vue
+++ src/components/common/ButtonWithIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseButton :type="type" :disabled="disabled" :size="size">
     <div class="flex items-center">
-      <slot name="icon"/>
+      <fa class="text-blue-400 mr-3" :size="size" :icon="icon"/>
       <slot name="text"/>
     </div>
   </BaseButton>
@@ -9,14 +9,23 @@
 
 <script>
 import BaseButton from "../../components/common/BaseButton";
+import Fa from 'vue-fa'
+import { faStar, faFlag, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export default {
   name: "ButtonWithIcon",
-  components: {BaseButton},
+  components: {BaseButton, Fa},
+  setup() {
+    return {
+      faStar, faFlag, faPlus
+    }
+  },
   props: {
     disabled: {
       type: Boolean,
-      default: false,
+    },
+    icon: {
+      type: Object,
     },
     type: {
       type: String,
@@ -29,6 +38,9 @@ export default {
       type: String,
       default: '',
     },
-  }
+    created() {
+      console.log(this.icon)
+    },
+  },
 }
 </script>
--- src/stories/common/ButtonWithIcon.stories.js
+++ src/stories/common/ButtonWithIcon.stories.js
@@ -23,11 +23,8 @@ const Template = (args) => ({
         return { faStar, faFlag, faPlus, args };
     },
     template:
-        '<button-with-icon v-bind="args">' +
+        '<button-with-icon v-bind="args" :icon="faStar">' +
             '<template #text>Test Button</template>' +
-            '<template #icon>' +
-                '<fa class="text-blue-400 mr-3" size="2x" :icon="faFlag"/>' +
-            '</template>' +
         '</button-with-icon>',
 });
 
