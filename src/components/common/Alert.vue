475002a534e2d260467b7e988da9b32d4ec9639c
Ben Kaiser <benediktkaiser8@gmail.com>
2021-09-12 14:07:12 +0200

    Fixed Codacy issues

--- src/components/common/Alert.vue
+++ src/components/common/Alert.vue
@@ -2,7 +2,9 @@
   <div class="alert">
     <div class="flex flex-col md:items-center md:flex-row">
       <div class="mr-3 p-4 rounded rounded-tr-none rounded-br-none text-white hidden md:block" :class="type">
-        <fa size="lg" :icon="icon"/>
+        <fa
+            size="lg"
+            :icon="icon" />
       </div>
       <p class="md:mr-2 text-base font-bold text-gray-800 capitalize hidden md:block">
         {{ type }}
@@ -11,10 +13,13 @@
       <slot class="text-sm lg:text-base text-gray-600 lg:pt-1 xl:pt-0 sm:mb-0 mb-2 sm:text-left" />
     </div>
     <div class="flex xl:items-center lg:items-center sm:justify-end justify-center pr-4">
-      <router-link :to="link" class="focus:outline-none focus:text-indigo-400 hover:text-indigo-400 text-sm mr-4 font-bold cursor-pointer text-indigo-700 dark:text-indigo-600">
+      <router-link :to="link"
+                   class="focus:outline-none focus:text-indigo-400 hover:text-indigo-400 text-sm mr-4 font-bold cursor-pointer text-indigo-700 dark:text-indigo-600">
         Details
       </router-link>
-      <button @click="$emit('dismiss')" class="focus:outline-none focus:text-gray-400 hover:text-gray-400 text-sm cursor-pointer text-gray-600 dark:text-gray-400">
+      <button
+          class="focus:outline-none focus:text-gray-400 hover:text-gray-400 text-sm cursor-pointer text-gray-600 dark:text-gray-400"
+          @click="$emit('dismiss')">
         Dismiss
       </button>
     </div>
@@ -43,12 +48,12 @@ export default {
       default: '',
     }
   },
+  emits: ['dismiss'],
   setup() {
     return {
       faCheckCircle, faInfoCircle, faTimesCircle, faExclamationCircle
     }
   },
-  emits: ['dismiss'],
   computed: {
     icon() {
       switch(this.type) {
